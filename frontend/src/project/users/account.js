79473fc9bf5ba59bba7e520825fe3550936f7df6
Richard Beaver <rickybeaver3@gmail.com>
2023-12-13 12:09:09 -0500

    Better layout on account page

--- frontend/src/project/users/account.js
+++ frontend/src/project/users/account.js
@@ -40,37 +40,76 @@ function Account() {
       {user && (
         <div>
           <p>Username: {user.username}</p>
-          <input
-            type="email"
-            className="form-control"
-            value={user.email}
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
-          />
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-            value={user.firstName}
-            onChange={(e) => setUser({ ...user, firstName: e.target.value })}
-          />
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Last name"
-            value={user.lastName}
-            onChange={(e) => setUser({ ...user, lastName: e.target.value })}
-          />
-          <button onClick={updateUser} className="btn btn-primary">
-            Update
-          </button>
-          <button onClick={signout} className="btn btn-danger">
-            Sign Out
-          </button>
-          {user.role === "ADMIN" && (
-            <Link to="/project/users" className="btn btn-warning">
-              Users
-            </Link>
-          )}
+
+          {/* Email */}
+          <div className="form-group row">
+            <label htmlFor="input-email" className="col-sm-2 col-form-label">
+              Email
+            </label>
+            <div className="col-sm-10">
+              <input
+                type="email"
+                className="form-control"
+                id="input-email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+              />
+            </div>
+          </div>
+
+          {/* First name */}
+          <div className="form-group row">
+            <label
+              htmlFor="input-first-name"
+              className="col-sm-2 col-form-label"
+            >
+              First name
+            </label>
+            <div className="col-sm-10">
+              <input
+                type="text"
+                className="form-control"
+                id="input-first-name"
+                value={user.firstName}
+                onChange={(e) =>
+                  setUser({ ...user, firstName: e.target.value })
+                }
+              />
+            </div>
+          </div>
+
+          {/* Last name */}
+          <div className="form-group row">
+            <label
+              htmlFor="input-last-name"
+              className="col-sm-2 col-form-label"
+            >
+              Last name
+            </label>
+            <div className="col-sm-10">
+              <input
+                type="text"
+                className="form-control"
+                id="input-last-name"
+                value={user.lastName}
+                onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+              />
+            </div>
+          </div>
+
+          <div className="py-2">
+            <button onClick={updateUser} className="btn btn-primary">
+              Update
+            </button>
+            <button onClick={signout} className="btn btn-danger">
+              Sign Out
+            </button>
+            {user.role === "ADMIN" && (
+              <Link to="/project/users" className="btn btn-warning">
+                Users
+              </Link>
+            )}
+          </div>
 
           <Interactions id={user._id} />
         </div>
